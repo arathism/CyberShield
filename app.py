5bc0bee45badaf3d5788cdfdaef68ee02287ed03
Arathi  Shekhar Munavalli <arathism74@gmail.com>
2026-03-13 15:51:29 +0530

    Fix indentation for server run block

--- app.py
+++ app.py
@@ -213,5 +213,5 @@ def test_sqli():
 
 # ---------------- RUN SERVER ----------------
 if __name__ == '__main__':
-   port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
